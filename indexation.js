f115c37bd1e4f57b4cea2f019452080eb97c7fc0
eloi-menaud <eloi-menaud@users.noreply.github.com>
2025-08-04 16:05:48 +0000

    deploy: 79fa155772e8da2c77f29d90bf3d549fd12698f5

--- indexation.js
+++ indexation.js
@@ -1,12 +1,12 @@
 const indexation = [
   {
-    "id": "00000",
-    "title": "A Test",
-    "tech": "Shell"
-  },
-  {
     "id": "00002",
     "title": "Flow Control",
     "tech": "Shell"
+  },
+  {
+    "id": "00000",
+    "title": "Squash commits",
+    "tech": "Git"
   }
 ]
